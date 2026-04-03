811ad6c08e6703f9a66cfc3b8423e0331cbb25ea
devanjali97 <devanjali97@gmail.com>
2026-04-02 18:09:04 -0700

    xy - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,6 +25,11 @@ public class DeveloperTest {
         assertEquals("Anjali", Developer.getName());
     }
 
+    @Test
+    public void etGithubId_returns_correct_githubId() {
+        assertEquals("dev-anj", Developer.getGithubId());
+    }
+
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
